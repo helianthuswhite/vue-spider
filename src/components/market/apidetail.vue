edd26ea5de1253577625a84c230906d48589ea91
huangyunzhi <huangyunzhi@baidu.com>
2017-06-13 20:41:23 +0800

    api-details c

--- src/components/market/apidetail.vue
+++ src/components/market/apidetail.vue
@@ -40,8 +40,116 @@
       </div>
       <div class="details">
         <el-tabs v-model="activeName" type="card">
-          <el-tab-pane label="API" name="first">
-            
+          <el-tab-pane label="API" name="first" class="api-use">
+            <div class="api-use-panel">
+              <div class="row">
+                接口地址： http://helianthuswhite.cn/api/getPic
+              </div>
+              <div class="row">
+                请求方法： POST
+              </div>
+              <div class="row">
+                请求参数（header）： 
+                <el-table
+                  :data="apiHeaderTableData"
+                  border
+                  style="width: 100%" class="api-table">
+                  <el-table-column
+                    prop="name"
+                    label="参数名称">
+                  </el-table-column>
+                  <el-table-column
+                    prop="type"
+                    label="参数类型">
+                  </el-table-column>
+                  <el-table-column
+                    prop="must"
+                    label="必填">
+                  </el-table-column>
+                  <el-table-column
+                    prop="locat"
+                    label="参数位置">
+                  </el-table-column>
+                  <el-table-column
+                    prop="desc"
+                    label="描述">
+                  </el-table-column>
+                  <el-table-column
+                    prop="default"
+                    label="默认值">
+                  </el-table-column>
+                </el-table>
+              </div>
+              <div class="row">
+                请求参数（bodyParam）： 
+                <el-table
+                  :data="apiBodyTableData"
+                  border
+                  style="width: 100%" class="api-table">
+                  <el-table-column
+                    prop="name"
+                    label="参数名称">
+                  </el-table-column>
+                  <el-table-column
+                    prop="type"
+                    label="参数类型">
+                  </el-table-column>
+                  <el-table-column
+                    prop="must"
+                    label="必填">
+                  </el-table-column>
+                  <el-table-column
+                    prop="locat"
+                    label="参数位置">
+                  </el-table-column>
+                  <el-table-column
+                    prop="desc"
+                    label="描述">
+                  </el-table-column>
+                  <el-table-column
+                    prop="default"
+                    label="默认值">
+                  </el-table-column>
+                </el-table>
+              </div>
+              <div class="row">
+                请求示例： 
+              </div>
+              <div class="row">
+                返回结果： 
+                <div class="content">
+                  {
+                      "errNum": "0",
+                      "errMsg": "success",
+                      "querySign": "3845925467,2370020290",
+                      "retData": [
+                          {
+                              "rect": {
+                                  "left": "0",
+                                  "top": "0",
+                                  "width": "33",
+                                  "height": "31"
+                              },
+                              "word": "  8"
+                          }
+                      ]
+                  }
+
+                  errNum   : 标识处理是否成功, 0: 表示成功, 其它值, 表示失败
+                  errMsg   : 错误类型说明
+                  querySign: 本次请求用户传递原图或rl的唯一标示, 方便定位问题
+                  retData  : 返回内容集合
+                  rect     :  {left, right, width, height}该行文字所在的矩形区域坐标: left, right, width, height
+                  word     : 该行所识别出的文字
+                </div>
+              </div>
+              <div class="row">
+                备注： 
+                <div class="content">
+                  2016-5-10日起提供两种版本 v1 为原本使用的稳定版本，v2为升级版本，可以通过version参数进行选择，默认为v1版本同时即日起支持1M以内的大图片识别，对于原本支持的300K以内图片大小保持原有收费方案，对于大于300K，小于1M的图可以通过参数sizetype设定为big进行识别，但此时计费2次。即sizetype设定为small时为原本识别方案，仅识别300K以内的图片，sizetype设定为big时支持1M以内的图片识别，此时小于300K的图片计费1次，大于300K小于1M的计费2次。
+                </div>
+              </div>
+            </div>
           </el-tab-pane>
           <el-tab-pane label="联系商家" name="second" class="contact">
             <div class="title">联系方式</div>
@@ -65,7 +173,7 @@
           </el-tab-pane>
           <el-tab-pane label="套餐" name="third">
             <el-table
-              :data="tableData"
+              :data="priceTableData"
               border
               style="width: 100%">
               <el-table-column
@@ -82,8 +190,44 @@
               </el-table-column>
             </el-table>
           </el-tab-pane>
-          <el-tab-pane label="错误码参照" name="fourth">角色管理</el-tab-pane>
-          <el-tab-pane label="交易记录" name="fifth">定时任务补偿</el-tab-pane>
+          <el-tab-pane label="错误码参照" name="fourth">
+            <el-table
+              :data="errorTableData"
+              border
+              style="width: 100%">
+              <el-table-column
+                prop="name"
+                label="错误码">
+              </el-table-column>
+              <el-table-column
+                prop="time"
+                label="错误码返回">
+              </el-table-column>
+              <el-table-column
+                prop="price"
+                label="错误码返回说明">
+              </el-table-column>
+            </el-table>
+          </el-tab-pane>
+          <el-tab-pane label="交易记录" name="fifth">
+            <el-table
+              :data="boughtTableData"
+              border
+              style="width: 100%">
+              <el-table-column
+                prop="name"
+                label="购买用户">
+              </el-table-column>
+              <el-table-column
+                prop="time"
+                label="购买套餐">
+              </el-table-column>
+              <el-table-column
+                prop="price"
+                label="购买时间">
+              </el-table-column>
+            </el-table>
+          </el-tab-pane>
         </el-tabs>
       </div>
     </div>
@@ -102,8 +246,8 @@ export default {
   },
   data() {
     return {
-      activeName: '',
-      tableData: [{
+      activeName: 'first',
+      priceTableData: [{
         name: '牛逼闪闪的套餐一',
         time: '666次',
         price: '666元'
@@ -111,6 +255,61 @@ export default {
         name: '牛逼闪闪的套餐二',
         time: '66666次',
         price: '6661213元'
+      }],
+      errorTableData: [{
+        name: '牛逼闪闪的套餐一',
+        time: '666次',
+        price: '666元'
+      }, {
+        name: '牛逼闪闪的套餐二',
+        time: '66666次',
+        price: '6661213元'
+      }],
+      boughtTableData: [{
+        name: '牛逼闪闪的套餐一',
+        time: '666次',
+        price: '666元'
+      }, {
+        name: '牛逼闪闪的套餐二',
+        time: '66666次',
+        price: '6661213元'
+      }],
+      apiHeaderTableData: [{
+        name: 'apikey',
+        type: 'string',
+        must: '是',
+        locat: 'header',
+        desc: 'API密钥',
+        default: '默认值'
+      }],
+      apiBodyTableData: [{
+        name: 'apikey',
+        type: 'string',
+        must: '是',
+        locat: 'header',
+        desc: 'API密钥',
+        default: '默认值'
+      }, {
+        name: 'apikey',
+        type: 'string',
+        must: '是',
+        locat: 'header',
+        desc: 'API密钥',
+        default: '默认值'
+      }, {
+        name: 'apikey',
+        type: 'string',
+        must: '是',
+        locat: 'header',
+        desc: 'API密钥',
+        default: '默认值'
+      }, {
+        name: 'apikey',
+        type: 'string',
+        must: '是',
+        locat: 'header',
+        desc: 'API密钥',
+        default: '默认值'
       }]
     };
   }
@@ -219,6 +418,20 @@ export default {
               color: #fff;
               border-radius: 0;
               background: #20a0ff;
+            }
+          }
+        }
+        .api-use {
+          padding: 0px 15px;
+          border: solid 1px #EDEDED;
+          .row {
+            margin: 30px 0;
+            .api-table {
+              margin-top: 20px;
+            }
+            .content {
+              margin-top: 20px;
+              line-height: 25px;
             }
           }
         }
